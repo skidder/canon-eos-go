1a9267dc78a43a1abeb3a71b85615c115c4a4e97
Scott Kidder <kidder.scott@gmail.com>
2015-06-15 21:19:53 -0700

    Add function to get CameraModel array for connected cameras

--- eos/eos_client.go
+++ eos/eos_client.go
@@ -5,11 +5,13 @@ package eos
 #cgo LDFLAGS: -framework Cocoa -framework EDSDK
 #define __MACOS__ 1
 #include <EDSDK/EDSDK.h>
+#include <EDSDK/EDSDKTypes.h>
 #include <stdlib.h>
 */
 import "C"
 import (
 	"errors"
+	"unsafe"
 )
 
 type EOSClient struct{}
@@ -32,4 +34,55 @@ func (p *EOSClient) Close() (err error) {
 		err = errors.New("Error when terminating Canon SDK")
 	}
 	return
+}
+
+func (e *EOSClient) GetCameraModels() (cameras []CameraModel, err error) {
+	var eosCameraList *C.EdsCameraListRef
+	var eosError C.EdsError
+	var cameraCount int
+
+	// get a reference to the cameras list record
+	if eosError = C.EdsGetCameraList((*C.EdsCameraListRef)(unsafe.Pointer(&eosCameraList))); eosError != C.EDS_ERR_OK {
+		err = errors.New("Error when obtaining reference to list of cameras")
+		return
+	}
+	defer C.EdsRelease((*C.struct___EdsObject)(unsafe.Pointer(&eosCameraList)))
+
+	// get the number of cameras connected
+	if eosError = C.EdsGetChildCount((*C.struct___EdsObject)(unsafe.Pointer(eosCameraList)), (*C.EdsUInt32)(unsafe.Pointer(&cameraCount))); eosError != C.EDS_ERR_OK {
+		err = errors.New("Error when obtaining count of cameras")
+		return
+	}
+
+	// get details for each camera detected
+	for i := 0; i < cameraCount; i++ {
+		var eosCameraRef *C.EdsCameraRef
+		if eosError = C.EdsGetChildAtIndex((*C.struct___EdsObject)(unsafe.Pointer(eosCameraList)), (C.EdsInt32)(i), (*C.EdsBaseRef)(unsafe.Pointer(&eosCameraRef))); eosError != C.EDS_ERR_OK {
+			err = errors.New("Error when obtaining camera")
+			return
+		}
+		defer C.EdsRelease((*C.struct___EdsObject)(unsafe.Pointer(&eosCameraRef)))
+
+		var eosCameraDeviceInfo C.EdsDeviceInfo
+		if eosError = C.EdsGetDeviceInfo((*C.struct___EdsObject)(unsafe.Pointer(eosCameraRef)), (*C.EdsDeviceInfo)(unsafe.Pointer(&eosCameraDeviceInfo))); eosError != C.EDS_ERR_OK {
+			err = errors.New("Error when obtaining camera device info")
+			return
+		}
+
+		cameras = append(cameras, CameraModel{
+			szPortName:          C.GoString((*_Ctype_char)(&eosCameraDeviceInfo.szPortName[0])),
+			szDeviceDescription: C.GoString((*_Ctype_char)(&eosCameraDeviceInfo.szDeviceDescription[0])),
+			deviceSubType:       (uint32)(eosCameraDeviceInfo.deviceSubType),
+			reserved:            (uint32)(eosCameraDeviceInfo.reserved),
+		})
+	}
+
+	return
+}
+
+type CameraModel struct {
+	szPortName          string
+	szDeviceDescription string
+	deviceSubType       uint32
+	reserved            uint32
 }
--- eos/eos_client_test.go
+++ eos/eos_client_test.go
@@ -11,3 +11,21 @@ func TestInitializeAndClose(t *testing.T) {
 	assert.Nil(t, e.Initialize())
 	assert.Nil(t, e.Close())
 }
+
+// A single Canon T4i camera must be connected in order to run as expected.
+func TestGetCameraModels(t *testing.T) {
+	e := NewEOSClient()
+	e.Initialize()
+	defer e.Close()
+
+	models, err := e.GetCameraModels()
+	assert.NotNil(t, models)
+	assert.Equal(t, 1, len(models))
+	assert.Nil(t, err)
+
+	// verify values in the first model entry
+	assert.Equal(t, "Canon EOS REBEL T4i", models[0].szDeviceDescription)
+	assert.Equal(t, "0", models[0].szPortName)
+	assert.Equal(t, 2971958586, int(models[0].reserved))
+	assert.Equal(t, 1, int(models[0].deviceSubType))
+}
